8d8b17b537669cdd14253b97fb6400e5e0b9a375
David Delgadillo <daviddel.731@gmail.com>
2021-06-26 15:41:46 -0700

    Input Todo component done. Component sending and receiving data.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import './App.css';
+//Components
+import InputTodo from './components/InputTodos';
+
+
+function App() {
+  return <Fragment>
+    <div className="container">
+      <InputTodo />
+
+    </div>
+  </Fragment>
+}
+
+export default App;
