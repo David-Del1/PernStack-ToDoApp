da8238fd96a17454245a0c5bf098ade11f108622
David Delgadillo <daviddel.731@gmail.com>
2021-06-26 17:21:27 -0700

    Completed update todo and delete todo components.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 import './App.css';
 //Components
 import InputTodo from './components/InputTodos';
+import ListTodos from './components/ListTodos';
 
 
 function App() {
   return <Fragment>
     <div className="container">
       <InputTodo />
-
+      <ListTodos />
     </div>
   </Fragment>
 }
